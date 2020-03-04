083a06f5d1b1ce62d745c6c494c0236a8418fb4c
Alison Feldish <alison.feldish@gmail.com>
2020-03-03 19:43:09 -0500

    exercise 02

--- codingchallenge.js
+++ codingchallenge.js
@@ -5,10 +5,39 @@
 //function adding (x, y) {
 //    var a  = x+y
 
-function adding (a, b, c) {
+//function adding (a, b, c) {
     
-    var sum = a+b+c
+//    var sum = a+b+c
 
-    console.log (sum / 3)
+//    console.log (sum / 3)
 
-}
+//}
+
+
+
+// 2. Write a function that takes an array
+// of numbers as an argument. Then it lists
+// ONLY the numbers that are divisible by 3
+// If there are not such numbers console.log
+//(“Sorry, no 3-divisible numbers”).
+//(Think of push() method)
+
+
+//var array = [1, 2, 3, 4, 5, 6, 7, 8]
+
+//function numberArray (argument) {
+//    var empty = []
+//    for(var i = 0; i < argument.length; i++) {
+//        if (argument[i] % 3 === 0) {
+//            empty.push(argument[i])
+//        }
+        
+//    }
+//    if (empty.length === 0) {
+//        console.log("There are no numbers divisible by 3.")
+//    }
+//    else {
+//        console.log(empty)
+//    }
+
+//}
