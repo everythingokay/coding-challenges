7c963d0eadb90f1f3556a576b950e7664d2cf7f6
Alison Feldish <alison.feldish@gmail.com>
2020-03-03 19:37:44 -0500

    exercise 01

---
+++ codingchallenge.js
@@ -0,0 +1,14 @@
+// 1. Write a function that takes three arguments 
+// - all numbers - adds them together, split the result
+// in 3 and displays the result on the console.
+
+//function adding (x, y) {
+//    var a  = x+y
+
+function adding (a, b, c) {
+    
+    var sum = a+b+c
+
+    console.log (sum / 3)
+
+}
